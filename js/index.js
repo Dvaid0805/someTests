e868f1a3b00207fbf724fd091878ce9fc8f9c75f
Davyd Bredykhin <correodemimobil@gmail.com>
2021-09-12 15:32:07 +0200

    css changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $(".questions-block__visible-item").click(function() {
-        $(this).siblings(".questions-block__hidden-item").slideToggle(400);
-        $(this).children(".fas").toggleClass("fa-caret-up fa-caret-down")
+        $(this).siblings(".questions-block__hidden-item").slideToggle();
+        $(this).toggleClass("active");
+        // $(this).children(".fas").toggleClass("fa-caret-up fa-caret-down");
+
     });
 
 });
